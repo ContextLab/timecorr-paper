838cd8db2305423a0376bfb17ef8452e5f3aff1a
lucy <lucywowen@gmail.com>
2018-12-10 14:40:52 -0500

    change config paths

--- code/scripts/config.py
+++ code/scripts/config.py
@@ -12,8 +12,8 @@ if (socket.gethostname() == 'Lucys-MacBook-Pro-3.local') or (socket.gethostname(
     config['startdir'] = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))  # directory to start the job in
     config['template'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'run_job_local.sh')
 else:
-    config['datadir'] = '/idata/cdl/lowen/timecorr_paper/pieman'
-    config['workingdir'] = '/idata/cdl/lowen/timecorr_paper'
+    config['datadir'] = '/idata/cdl/lowen/timecorr_paper/pieman/data'
+    config['workingdir'] = '/idata/cdl/lowen/timecorr_paper/pieman'
     config['startdir'] = '/idata/cdl/lowen'
     config['template'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'run_job.sh')
 
@@ -23,7 +23,7 @@ config['lockdir'] = os.path.join(config['workingdir'], 'locks')
 config['resultsdir'] = os.path.join(config['workingdir'], 'results')
 
 # runtime options
-config['jobname'] = "simulation"  # default job name
+config['jobname'] = "timecorr_analysis"  # default job name
 config['q'] = "default"  # options: default, testing, largeq
 config['nnodes'] = 1  # how many nodes to use for this one job
 config['ppn'] = 1  # how many processors to use for this one job (assume 4GB of RAM per processor)
