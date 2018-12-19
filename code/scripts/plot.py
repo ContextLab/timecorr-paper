5dd9ad9a3398559b4a17944e5a3a31a9e8a1aa72
lucy <lucywowen@gmail.com>
2018-12-19 12:27:40 -0500

    changed plot to show weights and accuracy

--- code/scripts/plot.py
+++ code/scripts/plot.py
@@ -11,35 +11,21 @@ sns.set(style="whitegrid")
 
 def grouped_barplot(df, x, y, hue, title=None, outfile=None):
     fig, ax = plt.subplots()
-    g = sns.factorplot(x=x, y=y, hue=hue, data=df, size=6, kind="bar", estimator=np.mean, ci=95, n_boot=1000,
+    g = sns.factorplot(x=x, y=y, hue=hue, data=df, size=8, kind="bar", estimator=np.mean, ci=95, n_boot=1000,
                        palette="cubehelix", ax=ax, order=['intact', 'paragraph', 'word', 'rest'])
 
     sns.despine(ax=ax, left=True)
     ax.set_title(title)
     ax.set_ylabel(y)
     ax.set_xlabel(x)
-    l = ax.legend(loc='center right', bbox_to_anchor=(1.25, 0.5), ncol=1)
+    ax.set_ylim(0, .75)
+    l = ax.legend(loc='center right', bbox_to_anchor=(1.25, 0.75), ncol=2)
     l.set_title(hue)
     if not outfile:
         fig.show()
     else:
         fig.savefig(outfile, bbox_inches='tight')
 
-# def weights_barplot(df, x, y, hue, title=None, outfile=None):
-#     fig, ax = plt.subplots()
-#     g = sns.factorplot(x=x, y=y, hue=hue, data=df, size=6, kind="bar", estimator=np.mean, ci=95, n_boot=1000,
-#                        palette="cubehelix", ax=ax, order=['intact', 'paragraph', 'word', 'rest'])
-#
-#     sns.despine(ax=ax, left=True)
-#     ax.set_title(title)
-#     ax.set_ylabel(y)
-#     ax.set_xlabel(x)
-#     l = ax.legend(loc='center right', bbox_to_anchor=(1.25, 0.5), ncol=1)
-#     l.set_title(hue)
-#     if not outfile:
-#         fig.show()
-#     else:
-#         fig.savefig(outfile, bbox_inches='tight')
 
 
 if len(sys.argv) < 2:
@@ -78,19 +64,27 @@ for p in params:
 
     melted_df = pd.DataFrame()
 
-    for c in np.arange(full_data['level'].max() + 1):
-        melted_temp_df = pd.DataFrame()
-        melted_temp_df['weights'] = full_data['level_' + str(c)]
-        melted_temp_df['level'] = full_data['level']
-        melted_temp_df['level'] = c
-        melted_temp_df['accuracy'] = full_data['accuracy']
-        melted_temp_df['cond'] = full_data['cond']
+    for c in np.arange(full_data['level'].max() + 2):
+        if c!=full_data['level'].max() + 1:
+            melted_temp_df = pd.DataFrame()
+            melted_temp_df['weights'] = full_data['level_' + str(c)]
+            melted_temp_df['level'] = full_data['level']
+            melted_temp_df['level'] = c
+            #melted_temp_df['accuracy'] = full_data['accuracy']
+            melted_temp_df['cond'] = full_data['cond']
 
-        if melted_df.empty:
-            melted_df = melted_temp_df
+            if melted_df.empty:
+                melted_df = melted_temp_df
+            else:
+                melted_df = melted_df.append(melted_temp_df)
         else:
-            melted_df = melted_df.append(melted_temp_df)
 
+            melted_temp_df = pd.DataFrame()
+            melted_temp_df['weights'] = full_data['accuracy']
+            melted_temp_df['level'] = full_data['level']
+            melted_temp_df['level'] = 'accuracy'
+            melted_temp_df['cond'] = full_data['cond']
+            melted_df = melted_df.append(melted_temp_df)
     full_data['error'] = 1-full_data['error']
 
     p_split = param_name.split('_')
