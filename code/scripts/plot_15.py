e8ff4af478dc3ef007ed9cb7cd4aabad2aba0105
Lucy Owen <lucyowen@vpn-investment-office-231-132-37.dartmouth.edu>
2021-07-09 13:41:07 -0400

    code cleanup for replication with source data

--- code/scripts/plot_15.py
+++ code/scripts/plot_15.py
@@ -1,13 +1,19 @@
 
 import supereeg as se
 import os
+import numpy as np
 from visbrain.objects import BrainObj, SceneObj, SourceObj, ColorbarObj
 
 cmap = 'autumn_r'
 
-neurosynth_dir ='../figs/neurosynth_data'
+replication_dir ='../figs/replication/'
+neurosynth_dir = os.path.join(replication_dir, 'neurosynth_data')
+fig_dir = replication_dir
 
-fig_dir = '../figs'
+#
+# neurosynth_dir ='../figs/neurosynth_data'
+#
+# fig_dir = '../figs'
 
 bo_dir = os.path.join(neurosynth_dir, 'bos')
 
@@ -19,68 +25,112 @@ conditions = ['intact', 'paragraph', 'rest', 'word']
 
 for c in conditions:
 
+    data1 = np.load(os.path.join(bo_dir,f'{c}_1_data_largest_abs.npy'))
+    xyz1 = np.load(os.path.join(bo_dir, f'{c}_1_locs_largest_abs.npy'))
 
-    b1 = se.load(os.path.join(bo_dir,f'{c}_1_largest_abs.bo'))
-    b2 = se.load(os.path.join(bo_dir,f'{c}_2_largest_abs.bo'))
-    b3 = se.load(os.path.join(bo_dir,f'{c}_3_largest_abs.bo'))
-    b4 = se.load(os.path.join(bo_dir,f'{c}_4_largest_abs.bo'))
-    b5 = se.load(os.path.join(bo_dir, f'{c}_5_largest_abs.bo'))
-    b6 = se.load(os.path.join(bo_dir, f'{c}_6_largest_abs.bo'))
-    b7 = se.load(os.path.join(bo_dir, f'{c}_7_largest_abs.bo'))
-    b8 = se.load(os.path.join(bo_dir, f'{c}_8_largest_abs.bo'))
-    b9 = se.load(os.path.join(bo_dir, f'{c}_9_largest_abs.bo'))
-    b10 = se.load(os.path.join(bo_dir, f'{c}_10_largest_abs.bo'))
-    b11 = se.load(os.path.join(bo_dir, f'{c}_11_largest_abs.bo'))
-    b12 = se.load(os.path.join(bo_dir, f'{c}_12_largest_abs.bo'))
-    b13 = se.load(os.path.join(bo_dir, f'{c}_13_largest_abs.bo'))
-    b14 = se.load(os.path.join(bo_dir, f'{c}_14_largest_abs.bo'))
-    b15 = se.load(os.path.join(bo_dir, f'{c}_15_largest_abs.bo'))
+    data2 = np.load(os.path.join(bo_dir, f'{c}_2_data_largest_abs.npy'))
+    xyz2 = np.load(os.path.join(bo_dir, f'{c}_2_locs_largest_abs.npy'))
 
+    data3= np.load(os.path.join(bo_dir, f'{c}_3_data_largest_abs.npy'))
+    xyz3 = np.load(os.path.join(bo_dir, f'{c}_3_locs_largest_abs.npy'))
 
-    data1 = b1.get_data().values.ravel()
-    xyz1 = b1.locs.values
+    data4 = np.load(os.path.join(bo_dir, f'{c}_4_data_largest_abs.npy'))
+    xyz4 = np.load(os.path.join(bo_dir, f'{c}_4_locs_largest_abs.npy'))
 
-    data2 = b2.get_data().values.ravel()
-    xyz2 = b2.locs.values
+    data5 = np.load(os.path.join(bo_dir,f'{c}_5_data_largest_abs.npy'))
+    xyz5 = np.load(os.path.join(bo_dir, f'{c}_5_locs_largest_abs.npy'))
 
-    data3 = b3.get_data().values.ravel()
-    xyz3 = b3.locs.values
+    data6 = np.load(os.path.join(bo_dir, f'{c}_6_data_largest_abs.npy'))
+    xyz6 = np.load(os.path.join(bo_dir, f'{c}_6_locs_largest_abs.npy'))
 
-    data4 = b4.get_data().values.ravel()
-    xyz4 = b4.locs.values
+    data7= np.load(os.path.join(bo_dir, f'{c}_7_data_largest_abs.npy'))
+    xyz7 = np.load(os.path.join(bo_dir, f'{c}_7_locs_largest_abs.npy'))
 
-    data5 = b5.get_data().values.ravel()
-    xyz5 = b5.locs.values
+    data8 = np.load(os.path.join(bo_dir, f'{c}_8_data_largest_abs.npy'))
+    xyz8 = np.load(os.path.join(bo_dir, f'{c}_8_locs_largest_abs.npy'))
 
-    data6 = b6.get_data().values.ravel()
-    xyz6 = b6.locs.values
+    data9 = np.load(os.path.join(bo_dir, f'{c}_9_data_largest_abs.npy'))
+    xyz9 = np.load(os.path.join(bo_dir, f'{c}_9_locs_largest_abs.npy'))
 
-    data7 = b7.get_data().values.ravel()
-    xyz7 = b7.locs.values
+    data10 = np.load(os.path.join(bo_dir,f'{c}_10_data_largest_abs.npy'))
+    xyz10 = np.load(os.path.join(bo_dir, f'{c}_10_locs_largest_abs.npy'))
 
-    data8 = b8.get_data().values.ravel()
-    xyz8 = b8.locs.values
+    data11 = np.load(os.path.join(bo_dir,f'{c}_11_data_largest_abs.npy'))
+    xyz11 = np.load(os.path.join(bo_dir, f'{c}_11_locs_largest_abs.npy'))
 
-    data9 = b9.get_data().values.ravel()
-    xyz9 = b9.locs.values
+    data12 = np.load(os.path.join(bo_dir, f'{c}_12_data_largest_abs.npy'))
+    xyz12 = np.load(os.path.join(bo_dir, f'{c}_12_locs_largest_abs.npy'))
 
-    data10 = b10.get_data().values.ravel()
-    xyz10 = b10.locs.values
+    data13= np.load(os.path.join(bo_dir, f'{c}_13_data_largest_abs.npy'))
+    xyz13 = np.load(os.path.join(bo_dir, f'{c}_13_locs_largest_abs.npy'))
 
-    data11 = b11.get_data().values.ravel()
-    xyz11 = b11.locs.values
+    data14 = np.load(os.path.join(bo_dir, f'{c}_14_data_largest_abs.npy'))
+    xyz14 = np.load(os.path.join(bo_dir, f'{c}_14_locs_largest_abs.npy'))
 
-    data12 = b12.get_data().values.ravel()
-    xyz12 = b12.locs.values
+    data15 = np.load(os.path.join(bo_dir,f'{c}_15_data_largest_abs.npy'))
+    xyz15 = np.load(os.path.join(bo_dir, f'{c}_15_locs_largest_abs.npy'))
 
-    data13 = b13.get_data().values.ravel()
-    xyz13 = b13.locs.values
-
-    data14 = b14.get_data().values.ravel()
-    xyz14 = b14.locs.values
-
-    data15 = b15.get_data().values.ravel()
-    xyz15 = b15.locs.values
+    # b1 = se.load(os.path.join(bo_dir,f'{c}_1_largest_abs.bo'))
+    # b2 = se.load(os.path.join(bo_dir,f'{c}_2_largest_abs.bo'))
+    # b3 = se.load(os.path.join(bo_dir,f'{c}_3_largest_abs.bo'))
+    # b4 = se.load(os.path.join(bo_dir,f'{c}_4_largest_abs.bo'))
+    # b5 = se.load(os.path.join(bo_dir, f'{c}_5_largest_abs.bo'))
+    # b6 = se.load(os.path.join(bo_dir, f'{c}_6_largest_abs.bo'))
+    # b7 = se.load(os.path.join(bo_dir, f'{c}_7_largest_abs.bo'))
+    # b8 = se.load(os.path.join(bo_dir, f'{c}_8_largest_abs.bo'))
+    # b9 = se.load(os.path.join(bo_dir, f'{c}_9_largest_abs.bo'))
+    # b10 = se.load(os.path.join(bo_dir, f'{c}_10_largest_abs.bo'))
+    # b11 = se.load(os.path.join(bo_dir, f'{c}_11_largest_abs.bo'))
+    # b12 = se.load(os.path.join(bo_dir, f'{c}_12_largest_abs.bo'))
+    # b13 = se.load(os.path.join(bo_dir, f'{c}_13_largest_abs.bo'))
+    # b14 = se.load(os.path.join(bo_dir, f'{c}_14_largest_abs.bo'))
+    # b15 = se.load(os.path.join(bo_dir, f'{c}_15_largest_abs.bo'))
+    #
+    #
+    # data1 = b1.get_data().values.ravel()
+    # xyz1 = b1.locs.values
+    #
+    # data2 = b2.get_data().values.ravel()
+    # xyz2 = b2.locs.values
+    #
+    # data3 = b3.get_data().values.ravel()
+    # xyz3 = b3.locs.values
+    #
+    # data4 = b4.get_data().values.ravel()
+    # xyz4 = b4.locs.values
+    #
+    # data5 = b5.get_data().values.ravel()
+    # xyz5 = b5.locs.values
+    #
+    # data6 = b6.get_data().values.ravel()
+    # xyz6 = b6.locs.values
+    #
+    # data7 = b7.get_data().values.ravel()
+    # xyz7 = b7.locs.values
+    #
+    # data8 = b8.get_data().values.ravel()
+    # xyz8 = b8.locs.values
+    #
+    # data9 = b9.get_data().values.ravel()
+    # xyz9 = b9.locs.values
+    #
+    # data10 = b10.get_data().values.ravel()
+    # xyz10 = b10.locs.values
+    #
+    # data11 = b11.get_data().values.ravel()
+    # xyz11 = b11.locs.values
+    #
+    # data12 = b12.get_data().values.ravel()
+    # xyz12 = b12.locs.values
+    #
+    # data13 = b13.get_data().values.ravel()
+    # xyz13 = b13.locs.values
+    #
+    # data14 = b14.get_data().values.ravel()
+    # xyz14 = b14.locs.values
+    #
+    # data15 = b15.get_data().values.ravel()
+    # xyz15 = b15.locs.values
 
     template_brain = 'B3'
 
